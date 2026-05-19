c4d05256dbe2e3d2e72c1ea615f954b0e4f151ed
Justin de Haas <justinh@supracom.nl>
2026-05-19 12:11:02 +0200

    Autopilot map toegevoegd en hier een script aan toe gevoegd voor het blokkeren en toelaten van apps downloaden tijdens autopilot.

---
+++ Intune/Autopilot/Allow.oR.Block.Win32.Apps.to.install.During.Autopilot.ESP.ps1
@@ -0,0 +1,14 @@
+﻿$ESP = Get-Process -ProcessName CloudExperienceHostBroker -ErrorAction SilentlyContinue
+
+If ($ESP) 
+
+     {
+    
+    Write-Host "Windows Autopilot ESP Running"
+    
+     }
+Else {
+    
+    Write-Host "Windows Autopilot ESP Not Running"
+    
+     }
